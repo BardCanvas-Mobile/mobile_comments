875bae6996c37dc8a9970865ea893cca6eac82d4
Alejandro Caballero <lava.caballero@gmail.com>
2017-12-14 12:58:09 -0600

    Added CORS headers

--- remote_edit.php
+++ remote_edit.php
@@ -20,7 +20,8 @@ use hng2_modules\mobile_controller\toolbox;
 
 include "../config.php";
 include "../includes/bootstrap.inc";
-header("Content-Type: text/plain; charset=utf-8");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
 
 $toolbox = new toolbox();
 $toolbox->output_type = "HTML";
